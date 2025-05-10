cc7399fc917d40792aac74c24b327e448ecba096
nkarimzade <nesibk8@gmail.com>
2025-05-10 15:18:11 +0300

    fix: Restore logo image in Navbar component

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => {
     return (
         <div>
             <div className="navbar">
-                
+                <div className="logo">
+                    <img src="/public/logo.jpg" alt="" />
+                </div>
                 <div className="name">
                     <h1>Nasib <span> Karimzade </span> </h1>
                 </div>
