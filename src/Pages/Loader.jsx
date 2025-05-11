a88880f264e71477a98d43952620778be3b7e6c2
nkarimzade <nesibk8@gmail.com>
2025-05-11 16:17:13 +0300

    fix: Remove load event listener and hide loading element logic from Loader component

--- src/Pages/Loader.jsx
+++ src/Pages/Loader.jsx
@@ -3,12 +3,7 @@ import "./Loader.css"
 
 const Loader = () => {
 
-    window.addEventListener('load', fg_load)
-
-    function fg_load() {
-        document.getElementById('loading').style.display = 'none'
-    }
-
+   
     return (
         <div id="loading">
             <img src="/public/load-7671.gif" alt="Yükleniyor..." />
