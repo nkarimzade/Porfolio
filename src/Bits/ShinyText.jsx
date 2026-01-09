e02bb0208715cbaf2d7cb1853d0718d3c98dc36f
nkarimzade <nesibk8@gmail.com>
2026-01-10 01:18:28 +0300

    Update v2

--- src/Bits/ShinyText.jsx
+++ src/Bits/ShinyText.jsx
@@ -1,5 +1,5 @@
 import { useState, useCallback, useEffect, useRef } from 'react';
-import { motion, useMotionValue, useAnimationFrame, useTransform } from 'motion/react';
+import {  useMotionValue, useAnimationFrame, useTransform } from 'motion/react';
 import './ShinyText.css';
 
 const ShinyText = ({
